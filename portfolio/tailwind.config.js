163250c42614a971ddfd0add14326ef7da86bf3b
nisharathod231 <nisharathod27899@gmail.com>
2024-05-25 12:19:51 +0530

    BoilerPlate Setup

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ['./index.html',"./src/**/*.{jsx,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
